915c9ed94de7fdf3478ecc319269ca96a4e4e54f
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-10-27 21:43:00 -0500

    implemented function to generate submission matrix

---
+++ docker/feature_extraction/reshape.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+
+def create_submission_matrix_from_query_result(fp):
+    """
+
+    :param fp:
+    :return:
+    """
+    df = pd.read_csv(fp, dtype=object)
+    quiz_submission_meta_colnames = ["id", "item_id", "session_user_id", "submission_time", "submission_number",
+                                     "raw_score", "grading_error", "authenticated_submission_id"]
+    pivot_index_colnames=["session_user_id", "id"]
+    df.columns = quiz_submission_meta_colnames
+    df["submission_time"] = df["submission_time"].astype(int)
+    df.pivot_table(index=pivot_index_colnames, columns="item_id", values="submission_time").reset_index()
+    # add prefix
+    df.rename(columns={x:"quiz_"+x for x in df.columns if x not in pivot_index_colnames})
+    df.to_csv()
+    return df
+
+
