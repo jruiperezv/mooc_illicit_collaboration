18810b879468eaaf89d9fb84520119c7bda903e0
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-10-27 21:43:51 -0500

    complete control flow for submission matrix creation

--- docker/mooc_illicit_collaboration.py
+++ docker/mooc_illicit_collaboration.py
@@ -1,4 +1,7 @@
 from feature_extraction.sql_utils import *
+from feature_extraction.reshape import create_submission_matrix_from_query_result
+import os
+import pandas as pd
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="execute feature extraction, training, or testing.")
@@ -11,7 +14,11 @@ if __name__ == "__main__":
     if args.mode == "extract":
         # this block expects individual session-level data mounted by extract_session() and outputs one CSV file per session in /output
         # generate forum post CSV files for each session and aggregate into single file
-        extract_coursera_sql_data(args.course, args.session)
-        #todo: postprocess sql results to form submission matrix by pivoting columns of csv
-    else:
+        raw_submission_matrix_filename = "submission_matrix_raw.csv"
+        extract_coursera_sql_data(args.course, args.session, outfile=raw_submission_matrix_filename)
+        # postprocess sql results to form submission matrix by pivoting columns of csv
+        submission_matrix = create_submission_matrix_from_query_result(raw_submission_matrix_filename)
+        submission_matrix.to_csv(os.path.join(course, session, "output", "submission_matrix.csv"))
+        os.remove(raw_submission_matrix_filename)
+    else: # this script should not be called in any other mode
         raise NotImplementedError
