5b54ad189cea3afa43f462ec2e276dfb63cab140
Josh Gardner <joshua.patrick.gardner@gmail.com>
2018-10-30 18:38:18 -0700

    keep only most recent for each user; use pd.pivot() instead of pd.pivot_table to ensure index is unique

--- docker/feature_extraction/reshape.py
+++ docker/feature_extraction/reshape.py
@@ -11,12 +11,14 @@ def create_submission_matrix_from_query_result(fp):
     df = pd.read_csv(fp, dtype=object)
     quiz_submission_meta_colnames = ["id", "item_id", "session_user_id", "submission_time", "submission_number",
                                      "raw_score", "grading_error", "authenticated_submission_id"]
-    pivot_index_colnames=["session_user_id", "id"]
+    pivot_index = "session_user_id"
     df.columns = quiz_submission_meta_colnames
     df["submission_time"] = df["submission_time"].astype(int)
-    df = df.pivot_table(index=pivot_index_colnames, columns="item_id", values="submission_time").reset_index()
+    # keep only most recent submission for each user https://stackoverflow.com/questions/32459325/python-pandas-dataframe-select-row-by-max-value-in-group/32459442
+    df = df.loc[df.groupby(pivot_index)["submission_time"].idxmax()]
+    df = df.pivot(index=pivot_index, columns="item_id", values="submission_time").reset_index()
     # add prefix
-    df.rename(columns={x:"quiz_"+x for x in df.columns if x not in pivot_index_colnames}, inplace=True)
+    df.rename(columns={x:"quiz_"+x for x in df.columns if x != pivot_index}, inplace=True)
     return df
 
 
